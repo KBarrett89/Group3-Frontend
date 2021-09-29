57159f383081c9e018493cd6d0cef74f3a5cba01
daryl-bishop <daryl.bishop@hotmail.com>
2021-09-29 17:35:43 +0100

    GROUP-117 minor css changes

--- src/components/Dashboard/PageNotFound.jsx
+++ src/components/Dashboard/PageNotFound.jsx
@@ -8,7 +8,7 @@ const PageNotFound = () => {
         <div id = "error">
             <h1 id = "em">404!</h1>
             <h3 id = "mess">Sorry, we can't find that page! Please try another search.</h3>
-            <Navbar/>
+            
         </div>
     )
     }
